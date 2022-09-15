9ce87a1f06082e6cc2cdf347b5bb774724553638
nlpander <113520520+nlpander@users.noreply.github.com>
2022-09-15 17:53:10 +0100

    tokenize functions cleaned

--- tokenize_tf.py
+++ tokenize_tf.py
@@ -1,17 +1,12 @@
 import spacy
 import pandas as pd
 import re
-import numpy as np
 from nltk.tokenize import TreebankWordTokenizer
 
-def f(x):
-    return x*x
 
-def HeaderAuthorFilter(text):
-    
+def WebItemsFilter(text):
     text = text.lower()
 
-    no_header = 0
     no_auth = 0
     no_em = 0
     no_tw = 0
@@ -19,87 +14,71 @@ def HeaderAuthorFilter(text):
     no_htmltag = 0
 
     try:
-        header = re.findall('\w+\s\(alliance news\)\s-\s',text)
-        text = text.replace(header[0],'')
-    except:
-        no_header = 1
-
-    try:
-        auth = re.findall('(by .*;)',text)
-        text = text.replace(auth[0],'')
+        auth = re.findall('(by .*;)', text)
+        text = text.replace(auth[0], '')
     except:
         no_auth = 1
 
     try:
-        em = re.findall('(\w+@\w+.com)',text)
-        text = text.replace(em[0],'')
+        em = re.findall('(\w+@\w+.com)', text)
+        text = text.replace(em[0], '')
     except:
         no_em = 1
 
     try:
-        tw = re.findall('(\s@\w+)',text)
-        text = text.replace(tw[0],'')
+        tw = re.findall('(\s@\w+)', text)
+        text = text.replace(tw[0], '')
     except:
         no_tw = 1
 
     try:
-        copyright = re.findall('copyright \d+ alliance news limited. all rights reserved.',text)
-        text = text.replace(copyright[0],'')
+        copyright = re.findall('copyright \d+ \w+. all rights reserved.', text)
+        text = text.replace(copyright[0], '')
     except:
         no_copy = 1
 
-    try:
-        copyright2 = re.findall('copyright dpa',text)
-        text = text.replace(copyright2[0],'')
-    except:
-        no_copy = 1
-
-    try:
-        copyright3 = re.findall('copyright rtt news/dpa-afx',text)
-        text = text.replace(copyright3[0],'')
-    except:
-        no_copy = 1
-
+    # removing html tags
     try:
         tags = re.findall('<[^>]*>', text)
         for tag in tags:
-            text = text.replace(tag,'')
+            text = text.replace(tag, '')
     except:
         no_htmltag = 1
 
     return text
 
+
 def NumericalExpressionFilter(word_list):
     # This also removes words that contain *any number*
-    #return [w for w in word_list if not any(c.isdigit() for c in w)]
-    for i in range(0,len(word_list)):
+    # return [w for w in word_list if not any(c.isdigit() for c in w)]
+    for i in range(0, len(word_list)):
 
-        #current word
+        # current word
         word = word_list[i]
 
-        #currency
-        currency = re.findall('(gbp(\d+))|(usd(\d+))|(eur(\d+))',word)
+        # currency
+        currency = re.findall('(gbp(\d+))|(usd(\d+))|(eur(\d+))', word)
 
-        #time match
-        time = re.findall('(\d+:\d+)',word)
+        # time match
+        time = re.findall('(\d+:\d+)', word)
 
-        #time period
-        period = re.findall('(\d+-day)|(\d+-week)|(\d+-month)|(\d+-year)',word)
+        # time period
+        period = re.findall('(\d+-day)|(\d+-week)|(\d+-month)|(\d+-year)', word)
 
-        #listed notation
-        listed = re.findall('\d+-listed',word)
+        # listed notation
+        listed = re.findall('\d+-listed', word)
 
-        #percentage/points 
-        perc = re.findall('\d+[p]',word)
+        # percentage/points
+        perc = re.findall('\d+[p]', word)
 
-        #xl then a number 
-        mult = re.findall('[xl]\d+',word)
+        # xl then a number
+        mult = re.findall('[xl]\d+', word)
 
-        #thousands
-        thou = re.findall('\d+,\d+',word)
-        
-        #dec
-        dec = re.findall('\d+.\d+',word)
+        # thousands
+        thou = re.findall('\d+,\d+', word)
+
+        # dec
+        dec = re.findall('\d+.\d+', word)
 
         if word.isdigit() or len(thou) != 0 or len(dec) != 0:
             word_list[i] = '#NUM'
@@ -120,7 +99,7 @@ def NumericalExpressionFilter(word_list):
             word_list[i] = '#TIMEPERIOD'
 
         if len(listed) != 0:
-            word_list[i] = '#LISTING'            
+            word_list[i] = '#LISTING'
 
     return word_list
 
@@ -128,24 +107,24 @@ def NumericalExpressionFilter(word_list):
 class Treebank_WordTokenize():
     def __init__(self):
         self.word_tokenizer = TreebankWordTokenizer()
-        self.stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 
-              'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 
-              'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 
-              'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 
-              'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 
-              'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 
-              'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 
-              'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 
-              'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 
-              'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
-              'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 
-              'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 
-              'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 
-              'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 
-              'd', 'll','\'ll', 'm', 'o', 're', 've','\'ve', 'y', 'ain', 'aren', 'couldn', 'didn', 
-              'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 
-              'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn','\'s','\'re']
-        
+        self.stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your',
+                           'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',
+                           'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+                           'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that',
+                           'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being',
+                           'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+                           'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
+                           'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+                           'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from',
+                           'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+                           'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why',
+                           'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other',
+                           'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
+                           'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now',
+                           'd', 'll', '\'ll', 'm', 'o', 're', 've', '\'ve', 'y', 'ain', 'aren', 'couldn', 'didn',
+                           'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn',
+                           'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn', '\'s', '\'re']
+
     def transform(self, text):
         words = []
         for w in self.word_tokenizer.tokenize(text):
@@ -155,122 +134,65 @@ class Treebank_WordTokenize():
                     words = words + ['#NUM']
                 elif w not in self.stop_words:
                     words = words + [w]
-                
+
         return words
 
-class Spacy_Tokenize():
+
+class Spacy_Tokenize:
     def __init__(self):
-        self.tokenizer = spacy.load('en_core_web_sm',disable=['tok2vec','attribute_ruler','lemmatizer'])
+        self.tokenizer = spacy.load('en_core_web_sm', disable=['tok2vec', 'attribute_ruler', 'lemmatizer'])
 
     def transform(self, text):
         words = []
-        for w in self.tokenizer(text):        
-            if ((w.is_stop == False) and (w.like_email == False)) and (w.like_url == False):
+        for w in self.tokenizer(text):
+            if ((w.is_stop is False) and (w.like_email is False)) and (w.like_url is False):
                 if w.ent_type_ != '':
                     words = words + [w.ent_type_]
                 else:
                     words = words + [w.text.lower()]
-                
+
         return words
 
-def SpacyTokenize_Job(df):
-    
-    spt = Spacy_Tokenize()
-    df.loc[:,'tokens'] = df.text.apply(lambda x:spt.transform(x))
-    
-    return df
 
-def SpacyTokenize_JobRay(df: pd.DataFrame) -> pd.DataFrame:
+class Filter_Tokenize:
 
-    spt = Spacy_Tokenize()
-    df.loc[:,'tokens'] = df.text.apply(lambda x:spt.transform(x))
-    
-    return df
+    def __init__(self, mode='treebank', text_column='body'):
 
-def TreebankTokenize_JobRay(df: pd.DataFrame) -> pd.DataFrame:
-    
-    twt = Treebank_WordTokenize()
-    df.loc[:,'tokens'] = df.text.apply(lambda x:twt.transform(x))
-    
-    return df
-
-class Filter_Tokenize():
-    
-    def __init__(self,mode='treebank',source='an'):
-        
         self.mode = mode
         self.tokenizer = None
-        self.source = source
-        
-    def transform(self,df):
-                        
-        if self.source == 'an':
-            
-            #assign sentiment label
-            df.loc[:,'label'] =  0.5
-            df.loc[df.sig_allpos == 1,'label'] = 1.0
-            df.loc[df.sig_allneg == 1,'label'] = 0.0
-       
-            #filter artices with tables
-            df = df[df.apply(lambda x:False if '<table' in x.body else True, axis = 1)].reset_index(drop=True)        
+        self.text_col = text_column
 
-            #remove filter authors and endings
-            df.loc[:,'body'] = df.body.apply(lambda x:HeaderAuthorFilter(x))
+    def transform(self, df):
 
-            #filter out header
-            df.loc[:,'headline'] = df.headline.apply(lambda x:' '.join(x.split(':')[1:]).lower() if len(x.split(':')) > 1\
-                      else x.lower())
-            
-        elif self.source == 'djx':
-            
-            #remove filter authors and endings
-            df.loc[:,'body'] = df.body.apply(lambda x:HeaderAuthorFilter(x))
+        # remove html tags / emails / websites
+        df.loc[:, self.text_col] = df[self.text_col].apply(lambda x: WebItemsFilter(x))
 
-        #make text column
-        df.loc[:,'text'] = df.apply(lambda x:' _HEADLINE_ ' + x.headline + ' _BODY_ ' + x.body, axis=1)
-        
-        #tokenize the text
+        # tokenize the text
         if self.mode == 'treebank':
-        
-            self.tokenizer = Treebank_WordTokenize()
-            df.loc[:,'tokens'] = df.loc[:,'text'].apply(lambda x:self.tokenizer.transform(x))
 
-            #filter numerical expression filter
-            df.loc[:,'tokens'] = df.tokens.apply(lambda x:NumericalExpressionFilter(x))
-        
+            self.tokenizer = Treebank_WordTokenize()
+            df.loc[:, 'tokens'] = df.loc[:, self.text_col].apply(lambda x: self.tokenizer.transform(x))
+
+            # translate numerical expressions
+            df.loc[:, 'tokens'] = df.tokens.apply(lambda x: NumericalExpressionFilter(x))
+
         elif self.mode == 'spacy':
-            
+
             self.tokenizer = Spacy_Tokenize()
-            df.loc[:,'tokens'] = df.text.apply(lambda x:self.tokenizer.transform(x))
-        
-        #translate to index and zero padded  
-        
+            df.loc[:, 'tokens'] = df.loc[:, self.text_col].apply(lambda x: self.tokenizer.transform(x))
+
         return df
 
-def SpacyFilterTokenize_AN_JobRay(df: pd.DataFrame) -> pd.DataFrame:
 
-    F = Filter_Tokenize(mode='spacy',source='an')
-    df = F.transform(df)
-    
+def SpacyTokenize_JobRay(df: pd.DataFrame) -> pd.DataFrame:
+    spt = Spacy_Tokenize()
+    df.loc[:, 'tokens'] = df.text.apply(lambda x: spt.transform(x))
+
     return df
 
-def TreebankFilterTokenize_AN_JobRay(df: pd.DataFrame) -> pd.DataFrame:
 
-    F = Filter_Tokenize(mode='treebank',source='an')
-    df = F.transform(df)
-    
-    return df
+def TreebankTokenize_JobRay(df: pd.DataFrame) -> pd.DataFrame:
+    twt = Treebank_WordTokenize()
+    df.loc[:, 'tokens'] = df.text.apply(lambda x: twt.transform(x))
 
-def SpacyFilterTokenize_DJX_JobRay(df: pd.DataFrame) -> pd.DataFrame:
-
-    F = Filter_Tokenize(mode='spacy',source='djx')
-    df = F.transform(df)
-    
-    return df
-
-def TreebankFilterTokenize_DJX_JobRay(df: pd.DataFrame) -> pd.DataFrame:
-
-    F = Filter_Tokenize(mode='treebank',source='djx')
-    df = F.transform(df)
-    
     return df
