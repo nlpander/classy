9ceaeb4a9dabf07e22eb8e0d26eebadfc9e53983
nlpander <113520520+nlpander@users.noreply.github.com>
2022-09-22 17:01:22 +0100

    setup - package files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='classy',
+    version='1.0.0pre',
+    author='nlpander',
+    author_email='tarafulis@gmail.com',
+    description='a package to do distributed preprocessing and training of neural networks for classification',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nlpander/classy',
+    project_urls = {
+        "Bug Tracker": "https://github.com/nlpander/classy/issues"
+    },
+    license='MIT',
+    packages=['classy'],
+    install_requires=['ray','numpy','pandas','torch','matplotlib','datasets','nltk','gensim','spacy'],
+)
