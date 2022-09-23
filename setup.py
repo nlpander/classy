87908b8eefee43f7e14770d8881411b611dff4cf
nlpander <113520520+nlpander@users.noreply.github.com>
2022-09-23 13:43:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/nlpander/classy/issues"
     },
     license='GNU',
-    packages=['classy','classy.preprocess','classy.train'],
+    packages=['classy','preprocess','train'],
     install_requires=['ray==1.13.0','numpy==1.22.4','pandas==1.4.2',
                     'torch==1.12.1+cu116','nltk==3.7',
                     'gensim==4.1.2','spacy==3.3.1','matplotlib',
