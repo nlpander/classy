2cb4a178908d4a22a2b71cf88c9e41c7bb58fa32
nlpander <113520520+nlpander@users.noreply.github.com>
2022-09-27 15:53:25 +0100

    changed spacy req - make break tokenization

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setuptools.setup(
     #packages = setuptools.find_packages(),
     packages = ['classy','classy.preprocess','classy.train'],
     install_requires=['ray==1.13.0','numpy','pandas',
-                    'torch==1.11.0','nltk==3.7',
-                    'gensim==4.1.2','spacy==3.3.1','matplotlib',
+                    'torch==1.11.0','nltk==3.7',spacy,
+                    'gensim==4.1.2','matplotlib',
                     'datasets',],
 )
+
+#'spacy==3.3.1'
