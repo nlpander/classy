3d8a3e522dcb16c39e75e79b20ef98e4b87b8f36
nlpander <113520520+nlpander@users.noreply.github.com>
2022-09-27 15:54:46 +0100

    typo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     #packages = setuptools.find_packages(),
     packages = ['classy','classy.preprocess','classy.train'],
     install_requires=['ray==1.13.0','numpy','pandas',
-                    'torch==1.11.0','nltk==3.7',spacy,
+                    'torch==1.11.0','nltk==3.7','spacy',
                     'gensim==4.1.2','matplotlib',
                     'datasets',],
 )
