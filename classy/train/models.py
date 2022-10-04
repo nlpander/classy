6e5b56763dd3dee4398beee2a81c1ed4a86e1b38
nlpander <113520520+nlpander@users.noreply.github.com>
2022-10-04 10:24:45 +0100

    changed the intermediate layer of the Conv 1D Network with Self Attention - now we can add variable transformer heads with variable self attention modules after the convolutional layers

--- classy/train/models.py
+++ classy/train/models.py
@@ -128,81 +128,9 @@ class Conv1D_Network_MultLabel(nn.Module):
         return output
 
 
-class Conv1D_Network_MultLabel_SA(nn.Module):
-
-    def __init__(self, seq_len, num_labels, embedding_matrix, hidden_units,
-                 freeze_embedding=True,
-                 filter_dimensions=(1, 3, 3, 5),
-                 number_filters=8,
-                 dropout=0.8):
-        super().__init__()
-
-        self.seq_len = seq_len
-        self.num_labels = num_labels
-
-        self.embedding_matrix = embedding_matrix
-        self.vocab_size = embedding_matrix.shape[0]
-        self.embedding_dim = embedding_matrix.shape[1]
-        self.freeze_embedding = freeze_embedding
-
-        self.filter_dim = filter_dimensions
-        self.number_filter_types = len(filter_dimensions)
-        self.number_filters = number_filters
-        self.hidden_units = hidden_units
-        self.dropout = dropout
-
-        self.Embedding = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embedding_dim, padding_idx=0). \
-            from_pretrained(embeddings=self.embedding_matrix, freeze=self.freeze_embedding).double()
-
-        self.PositionEmbedding = PositionalEncoding1D(channels=self.seq_len)
-        self.LN = nn.LayerNorm(self.seq_len)
-
-        self.ConvLayers = nn.ModuleList()
-
-        for ci in range(0, self.number_filter_types):
-            self.ConvLayers.append(nn.Conv1d(in_channels=self.embedding_dim, out_channels=self.number_filters,
-                                             kernel_size=self.filter_dim[ci],
-                                             padding=int((self.filter_dim[ci] - 1) / 2)).double())
-
-        self.Flatten = nn.Flatten()
-
-        self.Dropout = nn.Dropout(dropout)
-
-        self.SAHead = TransformerSelfAttentionHead(seq_len=self.seq_len,
-                                                   embed_dim=self.seq_len * self.number_filter_types * self.number_filters,
-                                                   hidden_units=self.hidden_units, dropout=dropout).double()
-
-        self.OutputLayer = nn.Linear(in_features=self.hidden_units, out_features=self.num_labels, bias=True).double()
-
-    def forward(self, seq):
-
-        y = self.Embedding(seq).transpose(1, 2)
-
-        # add the positional embedding
-        y = Summer(self.PositionEmbedding)(y)
-
-        # do a normalization
-        y = self.LN(y)
-
-        f = []
-        for ci in range(0, len(self.ConvLayers)):
-            f.append(self.ConvLayers[ci](y))
-
-        fc = torch.cat(tuple(f), 1)
-
-        fc_ = self.Flatten(fc)
-        # fc_ = self.Dropout(fc_)
-
-        sa_out = self.SAHead(fc_.reshape(tuple([1] + list(fc_.shape)))).reshape(fc_.shape[0], self.hidden_units)
-
-        output = torch.sigmoid(self.OutputLayer(sa_out))
-
-        return output
-
-
 class TransformerSelfAttentionHead(nn.Module):
 
-    def __init__(self, seq_len, embed_dim, hidden_units, dropout=0):
+    def __init__(self, seq_len, embed_dim, hidden_units, dropout=0.8):
         super().__init__()
 
         self.seq_len = seq_len
@@ -245,23 +173,23 @@ class TransformerSelfAttentionHead(nn.Module):
 
 class TransformerHead(nn.Module):
 
-    def __init__(self, seq_len, embedding_matrix, hidden_units, sa_heads=4, dropout=0):
+    def __init__(self, seq_len, embed_dim, hidden_units_sa, hidden_units_out, sa_heads=4, dropout=0.8):
         super().__init__()
 
         self.seq_len = seq_len
-        self.hidden_units = hidden_units
-        self.vocab_size = embedding_matrix.shape[0]
-        self.embed_dim = embedding_matrix.shape[1]
+        self.hidden_units_sa = hidden_units_sa
+        self.embed_dim = embed_dim
         self.sa_heads = sa_heads
+        self.hidden_units_out = hidden_units_out
         self.dropout = dropout
 
         self.SAHeads = nn.ModuleList()
 
         for mi in range(0, self.sa_heads):
             self.SAHeads.append(TransformerSelfAttentionHead(seq_len=self.seq_len, embed_dim=self.embed_dim,
-                                                             hidden_units=self.hidden_units, dropout=self.dropout))
+                                                             hidden_units=self.hidden_units_sa, dropout=self.dropout))
 
-        self.WZ = nn.Linear(in_features=self.hidden_units * self.sa_heads, out_features=self.embed_dim, bias=True)
+        self.WZ = nn.Linear(in_features=self.hidden_units_sa * self.sa_heads, out_features=self.hidden_units_out, bias=True)
         self.Dropout = nn.Dropout(self.dropout)
 
     def forward(self, inputA):
@@ -281,19 +209,20 @@ class TransformerHead(nn.Module):
 
 class ClassicTransformer(nn.Module):
 
-    def __init__(self, seq_len, hidden_units, embedding_matrix, num_labels,
-                 sa_heads=4, sa_modules=1, freeze_embedding=True,
-                 dropout=8, layer_norm=False):
-        super().__init_()
+    def __init__(self, seq_len, hidden_units_sa, hidden_units_out,
+                 embedding_matrix, num_labels, sa_heads=4, sa_modules=1,
+                 freeze_embedding=True, dropout=0.8, layer_norm=False):
+        super().__init__()
 
         self.seq_len = seq_len
-        self.hidden_units = hidden_units
+        self.hidden_units_sa = hidden_units_sa
+        self.hidden_units_out = hidden_units_out
+        self.sa_heads = sa_heads
+        self.sa_modules = sa_modules
         self.embedding_matrix = embedding_matrix
         self.num_labels = num_labels
         self.vocab_size = embedding_matrix.shape[0]
         self.embed_dim = embedding_matrix.shape[1]
-        self.sa_heads = sa_heads
-        self.sa_modules = sa_modules
         self.dropout = dropout
         self.layer_norm = layer_norm
 
@@ -304,9 +233,10 @@ class ClassicTransformer(nn.Module):
         self.TransformerHeads = nn.ModuleList()
 
         for ti in range(0, self.sa_modules):
-            self.TransformerHeads.append(TransformerHead(seq_len=self.seq_len, embedding_matrix=self.embedding_matrix,
-                                                         hidden_units=self.hidden_units, sa_heads=self.sa_heads,
-                                                         dropout=self.dropout))
+            self.TransformerHeads.append(TransformerHead(seq_len=self.seq_len, embed_dim=self.embed_dim,
+                                                         hidden_units_sa=int(self.hidden_units_sa / self.sa_heads),
+                                                         hidden_units_out=self.hidden_units_out,
+                                                         sa_heads=self.sa_heads, dropout=self.dropout))
 
         self.LN = nn.LayerNorm([self.seq_len, self.embed_dim])
 
@@ -314,7 +244,7 @@ class ClassicTransformer(nn.Module):
 
         self.Dropout = nn.Dropout(self.dropout)
 
-        self.OutputLayer = nn.Linear(in_features=self.embed_dim * self.seq_len, out_features=self.num_labels, bias=True)
+        self.OutputLayer = nn.Linear(in_features=self.hidden_units_out, out_features=self.num_labels, bias=True)
 
     def forward(self, seqs):
 
@@ -345,3 +275,119 @@ class ClassicTransformer(nn.Module):
         y = torch.sigmoid(y)
 
         return y
+
+
+class Conv1D_Network_MultLabel_SA(nn.Module):
+
+    def __init__(self, seq_len,
+                 num_labels,
+                 embedding_matrix,
+                 hidden_units_sa,
+                 hidden_units_out,
+                 freeze_embedding=True,
+                 filter_dimensions=(1, 3, 3, 5),
+                 number_filters=8,
+                 sa_modules=1,
+                 sa_heads=4,
+                 layer_norm=True,
+                 dropout=0.8):
+        super().__init__()
+
+        self.seq_len = seq_len
+        self.num_labels = num_labels
+
+        self.embedding_matrix = embedding_matrix
+        self.vocab_size = embedding_matrix.shape[0]
+        self.embedding_dim = embedding_matrix.shape[1]
+        self.freeze_embedding = freeze_embedding
+
+        self.filter_dim = filter_dimensions
+        self.number_filter_types = len(filter_dimensions)
+        self.number_filters = number_filters
+        self.sa_modules = sa_modules
+        self.sa_heads = sa_heads
+        self.hidden_units_sa = hidden_units_sa
+        self.hidden_units_out = hidden_units_out
+        self.layer_norm = layer_norm
+        self.dropout = dropout
+
+        self.Embedding = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embedding_dim, padding_idx=0). \
+            from_pretrained(embeddings=self.embedding_matrix, freeze=self.freeze_embedding)
+
+        self.PositionEmbedding = PositionalEncoding1D(channels=self.seq_len)
+        self.LN = nn.LayerNorm(self.seq_len)
+
+        self.ConvLayers = nn.ModuleList()
+
+        for ci in range(0, self.number_filter_types):
+            self.ConvLayers.append(nn.Conv1d(in_channels=self.embedding_dim, out_channels=self.number_filters,
+                                             kernel_size=self.filter_dim[ci],
+                                             padding=int((self.filter_dim[ci] - 1) / 2)))
+
+        self.Flatten = nn.Flatten()
+
+        self.Dropout = nn.Dropout(dropout)
+
+        # self.SAHead = TransformerSelfAttentionHead(seq_len=self.seq_len, embed_dim=self.seq_len *
+        # self.number_filter_types * self.number_filters, hidden_units=self.hidden_units, dropout=dropout).double()
+
+        self.TransformerHeads = nn.ModuleList()
+
+        for ti in range(0, self.sa_modules):
+            self.TransformerHeads.append(TransformerHead(seq_len=self.seq_len,
+                                                         embed_dim=self.seq_len * self.number_filter_types * self.number_filters,
+                                                         sa_heads=self.sa_heads,
+                                                         hidden_units_sa=int(self.hidden_units_sa / self.sa_heads),
+                                                         hidden_units_out=self.hidden_units_out,
+                                                         dropout=self.dropout))
+
+        self.LN1 = nn.LayerNorm([self.embedding_dim, self.seq_len])
+        self.LN2 = nn.LayerNorm(self.hidden_units_out)
+
+        self.OutputLayer = nn.Linear(in_features=self.hidden_units_out,
+                                     out_features=self.num_labels, bias=True)
+
+    def forward(self, seq):
+
+        y = self.Embedding(seq).transpose(1, 2)
+
+        # add the positional embedding
+        y = Summer(self.PositionEmbedding)(y)
+
+        # do a normalization
+        y = self.LN1(y)
+
+        f = []
+        for ci in range(0, len(self.ConvLayers)):
+            f.append(self.ConvLayers[ci](y))
+
+        fc = torch.cat(tuple(f), 1)
+
+        fc_ = self.Flatten(fc)
+
+        fc2 = fc_.reshape(tuple([1] + list(fc_.shape)))
+
+        # this may have to be reevaluated not sure if we need to add sequential skip connections
+        # between attention heads or simply with inputs
+        ZF = self.TransformerHeads[0](fc2)
+
+        if self.layer_norm:
+            X_ = self.LN2(ZF + fc2)
+        else:
+            X_ = ZF + fc2
+
+        if self.sa_modules > 1:
+
+            for ti in range(1,self.sa_modules):
+                ZF = self.TransformerHeads[ti](X_)
+
+            if self.layer_norm:
+                X_ = self.LN2(ZF + X_)
+            else:
+                X_ = ZF + X_
+
+        X_ = X_.reshape(fc_.shape[0], self.hidden_units_out)
+
+        output = torch.sigmoid(self.OutputLayer(X_))
+
+        return output
